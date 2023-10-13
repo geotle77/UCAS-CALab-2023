36989e13f57c4fa0dc2d6fae08ad052be4bb72f3
geotle77 <3396398733@qq.com>
2023-10-13 18:30:53 +0800

    some changes test in the IFstage

--- IFstage.v
+++ IFstage.v
@@ -2,15 +2,20 @@ module IFstage (
   input wire clk,
   input wire resetn,
   input wire reset,
+  
+  //the interface between Inst_SRAM to IFstage
   output wire inst_sram_en,
   output wire [3:0] inst_sram_we,
   output wire [31:0] inst_sram_addr,
   output wire [31:0] inst_sram_wdata,
   input wire [31:0] inst_sram_rdata,
+  //branch signal
   input wire br_taken,
   input wire [31:0] br_target,
+  //IF2ID bus
   output wire [31:0] inst,
   output wire [31:0] fs_pc,
+  //the interface between IFstage to IDstage
   output reg fs_valid,
   input wire ds_allowin,
   output wire fs2ds_valid
@@ -26,8 +31,8 @@ reg [31:0] pc;
 
 
 //////////pipeline////////
-wire fs_ready_go; // fs可以接收数据
-wire fs_allowin; // fs可以接收指令
+wire fs_ready_go; // 
+wire fs_allowin; //
 
 assign fs_ready_go = 1'b1;
 assign fs_allowin = ~fs_valid || fs_ready_go && ds_allowin;
@@ -38,13 +43,15 @@ assign fs_pc = pc;
 always @(posedge clk) begin
   if (~resetn) begin
     fs_valid <= 1'b0;
-  end else if (fs_allowin) begin
+  end 
+  else if (fs_allowin) begin
     fs_valid <= resetn;
   end
   
   if (~resetn) begin
     pc <= 32'h1bfffffc; // trick: to make nextpc be 0x1c000000 during reset
-  end else if (resetn && fs_allowin)begin
+  end 
+  else if (resetn & fs_allowin )begin
     pc <= nextpc;
   end
 end
--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 # CALab
-2023
+This project is the exp10  of the course of Computer Architecture.
+
+The exp10 is to implement **MUL** module and make it work.
